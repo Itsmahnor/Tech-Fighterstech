3c5b53083ec6ccc7bdc62b30bb8f405917f00298
Itsmahnor <mahnoorsarfraz23@gmail.com>
2025-07-02 20:57:58 +0500

    update home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import { AboutMe } from "../Components/AboutMe/AboutMe";
 import { Courses } from "../Components/Courses/Courses";
 import Footer from "../Components/Footer/Footer";
 import { Hero } from "../Components/Hero/Hero";
-import { ICONS } from "../Components/Icons/Icons";
+import { ICONS } from "../Components/Icons/Icons.jsx";
 import { BottomNav } from "../Components/Navbar/BottomNav";
 import { TopNav } from "../Components/Navbar/TopNav";
 import { TopCatagories } from "../Components/TopCatagories/TopCatagories";
